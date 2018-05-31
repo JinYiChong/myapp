b626f514f583afb1baaa15ded8cc2c5943e8e387
Jin <chongjinyi29@gmail.com>
2018-05-31 21:28:32 +0800

    added intent

--- app/src/main/java/com/example/jay/myapp/Welcome.kt
+++ app/src/main/java/com/example/jay/myapp/Welcome.kt
@@ -1,12 +1,20 @@
 package com.example.jay.myapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class Welcome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+
+        }
     }
 }
